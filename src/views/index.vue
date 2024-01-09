65a048a8aa5275521ed94839fd1a9b23cf37d72e
yym <yangym@golive.cc>
2024-01-09 14:11:58 +0800

    chore 川师微生活这几个字隐藏掉

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="divBox">
-    <div class="title">川师微生活数据大屏
+    <div class="title">AA大学生活数据大屏
       <!-- @click="toFullScreen" -->
       <div style="color: aliceblue;">{{ getDateDay() }} {{ getWeek() }}
         <!-- <i class="iocn clouds"></i> 多云  -->
